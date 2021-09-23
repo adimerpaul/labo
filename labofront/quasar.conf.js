82c3b9e4ad6e2f4a8f75c9dc4c46b8c1cf3f2d6d
adimerpaul <adimer101@email.com>
2021-09-22 22:57:31 -0400

    Update quasar.conf.js

--- labofront/quasar.conf.js
+++ labofront/quasar.conf.js
@@ -73,7 +73,7 @@ module.exports = configure(function (ctx) {
       env: {
         API: ctx.dev
           ? 'http://localhost:8000/api'
-          : 'http://165.22.188.126:92/api'
+          : 'http://165.22.188.126:90/api'
       }
     },
 
