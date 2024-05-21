830c0d5139fbec9b4d930f48d848973abbfcca61
@lopez.gutierrez.a07 <lopez.gutierrez.a07@gmail.com>
2024-05-20 20:23:47 -0400

    mod delete

--- laboback/app/Http/Controllers/InmunologiaController.php
+++ laboback/app/Http/Controllers/InmunologiaController.php
@@ -43,4 +43,42 @@ class InmunologiaController extends Controller
         }
 
     }
+    public function update(Inmunologia $inmunologia,Request $request){
+        //return $request;
+        $inmunologia=Inmunologia::find($request->id);
+        $inmunologia->tipomuestra= $request->tipomuestra==null?'':$request->tipomuestra;
+        $inmunologia->fechatoma= $request->fechatoma;
+        $inmunologia->horatoma= $request->horatoma;
+        $inmunologia->fechaimp= $request->fechaimp;
+        $inmunologia->responsable=  $request->responsable==null?'':$request->responsable;
+        $inmunologia->solicitud= $request->solicitud;
+//        $inmunologia->examenDirecto= $request->examendircto==null?'':$request->examendircto;
+//        $inmunologia->tincionGram= $request->tinciongram==null?'':$request->tinciongram;
+//        $inmunologia->microorganizmo= $request->microorganizmo==null?'':$request->microorganizmo;
+        $inmunologia->observacion= $request->observacion==null?'':$request->observacion;
+        $inmunologia->tipo_id=$request->tipo_id;
+        $inmunologia->paciente_id=$request->paciente_id;
+        $inmunologia->user_id=$request->user_id;
+        $inmunologia->doctor_id=$request->doctor_id;
+        $inmunologia->save();
+
+        DB::SELECT("DELETE from antibiotico_inmunologia where inmunologia_id=$inmunologia->id ");
+
+        foreach ($request->antibiograma as $value) {
+            $cultantib=new AntibioticoInmunologia();
+            $cultantib->inmunologia_id= $inmunologia->id;
+            $cultantib->antibiotico_id= $value['id'];
+            $cultantib->resultado= $value['interpretacion'];
+            $cultantib->save();
+        }
+
+    }
+
+    public function destroy($id){
+        $inmunologia = Inmunologia::find($id);
+
+        DB::SELECT("DELETE from antibiotico_inmunologia where inmunologia_id=$id ");
+        $inmunologia->delete();
+
+    }
 }
